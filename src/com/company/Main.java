1b21633ccaee2de6b4a35f83cb74998265a75b51
obidkon <obid.sharipov.80@mail.ru>
2020-11-03 23:24:13 +0500

    sacaner

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scanner =new Scanner(System.in);
+       //o System.out.println(scanner.nextInt());
+        //System.out.println(scanner.nextLine());
+        int kritilgan=1;
+        System.out.println("Hozirgi paytda");
+        System.out.println(kritilgan);
+        System.out.println(scanner.nextInt(18));
+    }
+}
